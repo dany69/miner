1b2f02f1ba852a56558218e93411dfe88e75d13c
dany69 <38370943+dany69@users.noreply.github.com>
2021-05-17 19:12:36 +0700

    Create mbc.sh

---
+++ mbc.sh
@@ -0,0 +1,5 @@
+# mbc
+sudo apt update
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a power2b -o stratum+tcp://stratum-eu.rplant.xyz:7022 -u Bq4GfPAJ7gYvqSryWHarQ1N28CUjP48RUP.test -t 2
